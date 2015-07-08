c12d2b721b43d504fb36bedfaee75d6a36034aad
Angus <angus@Angus-MacBook-Pro.local>
2015-07-08 23:11:10 +0800

    Init commit

---
+++ src/bg/background.js
@@ -0,0 +1,38 @@
+var inboardUrlScheme = 'inboard://import?url=';
+var imageCxtTtile = chrome.i18n.getMessage('saveImage');
+var pageCxtTitle  = chrome.i18n.getMessage('savePage');
+
+function handleImageUrl(url) {
+  var redirectUrl = inboardUrlScheme + url;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var tab = tabs[0];
+    // Open scheme url in current tab
+    chrome.tabs.update(tab.id, {url: redirectUrl});
+  });
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+
+  // Save image to Inboard
+  chrome.contextMenus.create({
+    'title': imageCxtTtile,
+    'contexts':['image'],
+    onclick: function(info) {
+      handleImageUrl(info.srcUrl);
+    }
+  });
+
+  // Save page to Inboard
+  chrome.contextMenus.create({
+    'title': pageCxtTitle,
+    'contexts':['page'],
+    onclick: function(info, tab) {
+      handleImageUrl(tab.url);
+    }
+  });
+});
+
+// Save page to Inboard
+chrome.browserAction.onClicked.addListener(function(tab) {
+  handleImageUrl(tab.url);
+});

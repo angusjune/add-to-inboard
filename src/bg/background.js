7300eda5799ae6fd8716dbb1cd3a6ea16a05d77c
Angus Zhu <angus.is.online@gmail.com>
2015-09-02 11:16:28 +0800

    Added title and web_url parameters

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,15 +1,23 @@
 var inboardUrlScheme = 'inboard://import?url=';
 var imageCxtTtile = chrome.i18n.getMessage('saveImage');
 var pageCxtTitle  = chrome.i18n.getMessage('savePage');
+var tabTitle;
+var tabUrl;
 
 function handleImageUrl(url) {
-  var redirectUrl = inboardUrlScheme + url;
+  var redirectUrl = inboardUrlScheme + url + '&title=' + encodeURI(tabTitle) + '&web_url=' + encodeURI(tabUrl);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var tab = tabs[0];
     // Open scheme url in current tab
     chrome.tabs.update(tab.id, {url: redirectUrl});
   });
 }
+
+chrome.tabs.getSelected(null,function(tab) {
+  // Get current tab title
+  tabTitle = tab.title;
+  tabUrl   = tab.url;
+});
 
 // Save image to Inboard
 chrome.contextMenus.create({

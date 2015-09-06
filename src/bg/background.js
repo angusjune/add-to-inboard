f49379c71752e40078b6e25871cf38822e881d37
Angus Zhu <angus.is.online@gmail.com>
2015-09-06 22:20:06 +0800

    Fix the bug that will pass wrong parameters to url scheme

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,23 +1,18 @@
 var inboardUrlScheme = 'inboard://import?url=';
 var imageCxtTtile = chrome.i18n.getMessage('saveImage');
 var pageCxtTitle  = chrome.i18n.getMessage('savePage');
-var tabTitle;
-var tabUrl;
 
 function handleImageUrl(url) {
-  var redirectUrl = inboardUrlScheme + url + '&title=' + encodeURI(tabTitle) + '&web_url=' + encodeURI(tabUrl);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var tab = tabs[0];
-    // Open scheme url in current tab
+    var tab    = tabs[0];
+    var title  = tab.title;
+    var tabUrl = tab.url
+
+    var redirectUrl = inboardUrlScheme + url + '&title=' + encodeURI(title) + '&web_url=' + encodeURI(tabUrl);
+
     chrome.tabs.update(tab.id, {url: redirectUrl});
   });
 }
-
-chrome.tabs.getSelected(null,function(tab) {
-  // Get current tab title
-  tabTitle = tab.title;
-  tabUrl   = tab.url;
-});
 
 // Save image to Inboard
 chrome.contextMenus.create({
